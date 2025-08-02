51c758359f6fe1587144399cc699887a461fa996
Ratnesh Kumar Jaiswal <155527162+RatneshKJaiswal@users.noreply.github.com>
2025-08-02 17:53:15 +0530

    19: feat: implement meetings filters and UI components, including agent and status filters, search functionality, and pagination

---
+++ src/modules/meetings/ui/components/agent-id-filter.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { CommandSelect } from "@/components/command-select";
+import { GenerateAvatar } from "@/components/generate-avatar";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+import { Divide } from "lucide-react";
+
+export const AgentIdFilter = () => {
+    const [ filters, setfilters] = useMeetingsFilters();
+
+    const trpc = useTRPC();
+
+    const [agentSearch, setAgentSearch] = useState<string>("");
+
+    const {data} = useQuery(
+        trpc.agents.getMany.queryOptions({
+            pageSize:100,
+            search:agentSearch,
+        })
+    );
+
+    return(
+        <CommandSelect
+            className="h-9"
+            placeholder="Agent"
+            options={(data?.items ?? []).map((agent) => ({
+                id: agent.id,
+                value: agent.id,
+                children: (
+                    <div className="flex items-center gap-x-2">
+                        <GenerateAvatar
+                            seed={agent.name}
+                            variant="botttsNeutral"
+                            className="size-4"
+                        />
+                        {agent.name}
+                    </div>
+                )
+            }))}
+            onSelect={(value) => setfilters({agentId: value})}
+            onSearch={setAgentSearch}
+            value={filters.agentId ?? ""}
+        />
+    );
+};
